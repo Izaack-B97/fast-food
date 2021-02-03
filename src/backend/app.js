3a7ae46be13fb67bd9529b5269de6c11205a704e
Isaac Bustamante <izaack.97@gmail.com>
2021-02-03 10:18:27 -0700

    Stage ready

---
+++ src/backend/app.js
@@ -0,0 +1,15 @@
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+
+app.use( bodyParser.urlencoded({ extended: true }) ); 
+app.use( bodyParser.json() );
+
+app.listen(port, () => {
+    console.log('Server on port ' + port);
+});
+
