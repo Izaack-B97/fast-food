aa903084a965d99c524567a21625f38ec4596cf5
Isaac Bustamante <izaack.97@gmail.com>
2021-02-20 12:06:53 -0700

    Ya esta las graficas solo para jalar la  info que corresponde

--- src/public/js/graficos.js
+++ src/public/js/graficos.js
@@ -7,25 +7,42 @@ const { getToServer } = require('./js/helpers/llamadas');
 
     getToServer('ordenes')
         .then(ordenes => {
-            
-            const mapFechas = ordenes.map(orden => moment( orden.create_at ).format('DD-MM-YYYY') ); 
+            console.log( ordenes )
+
+            const mapFechas = ordenes.map(orden => moment( orden.create_at ).format('DD-MM-YYYY') );
             const mapGanancias = ordenes.map(orden =>  orden.total_pagar );
 
-            console.log( mapFechas );
-            console.log(  mapGanancias );
+            // console.log( mapFechas );
+            // console.log(  mapGanancias );
 
-            const valorTotal = mapGanancias.reduce((anterior, actual) => anterior + actual );
+            const gananciasTotales = mapGanancias.reduce((anterior, actual) => anterior + actual );
             
-            const columnas = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-            const data = [12, 19, 3, 5, 2, 3];
+            // const columnas = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+            // const data = [12, 19, 3, 5, 2, 3];
 
             const divGrafica = document.querySelector('#myChart');
-            render_grafico( divGrafica, 'doughnut', columnas,  data);
+            render_grafico( divGrafica, 'pie', [ 'Ingresos', 'Impuestos' ],  [ gananciasTotales, 100 ]);
         
         })
         .catch(err => {
             console.log(err);
         })
 
+    getToServer('ordenes/informacion/general/todas')
+        .then(data => {
+            const divGraficaDogos = document.querySelector('#myChartDogos')
+            const divGraficaBebidas = document.querySelector('#myChartBebidas')
+            const divGraficaMomias = document.querySelector('#myChartMomias')
+            const divGraficaHamburguesas = document.querySelector('#myChartHamburguesas')
+
+            render_grafico( divGraficaDogos, 'doughnut', '',  [ 1200, 100 ]);
+            render_grafico( divGraficaBebidas, 'doughnut', '',  [ 1200, 100 ]);
+            render_grafico( divGraficaMomias, 'doughnut', '',  [ 1200, 100 ]);
+            render_grafico( divGraficaHamburguesas, 'doughnut', '',  [ 1200, 100 ]);
+            
+        })
+        .catch(err => {
+            console.log( err );
+        });
 
 })();
