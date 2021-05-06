f3e3d35ab6f78268830a0dcb2d66e6f0dfe0e1a3
Isaac Bustamante <izaack.97@gmail.com>
2021-05-05 18:13:55 -0700

    modulo de almacen

--- src/backend/controllers/almacen-controller.js
+++ src/backend/controllers/almacen-controller.js
@@ -3,20 +3,98 @@ const { getConnection } =  require('../database');
 
 module.exports = {
  
-    getAlmacen: ( req, res ) => {
-        res.json('Todos los productos en almacen');
+    getAlmacen: async ( req, res ) => {
+        try {
+            const query = `SELECT * FROM almacen`;
+
+            const conn = await getConnection();
+            const result =  await conn.query( query );
+
+            res.json( result );
+        } catch (error) {
+            res.json({
+                status: 'failed',
+                message: 'Error al obtener el almacen: ' + error
+            });
+        }
     },
-    createAlmacenProducto: ( req, res ) => {
-        res.json('Producto en almacen creado')
+    createAlmacenProducto: async ( req, res ) => {
+        try {
+            const query = `
+                INSERT INTO almacen 
+                    (
+                        producto,
+                        tipo_producto,
+                        cantidad
+                    )
+                VALUES
+                    (
+                        "${ req.body.producto }", 
+                        "${ req.body.tipo_producto }",
+                        ${ req.body.cantidad }
+                    );
+            `;
+
+            const conn = await getConnection();
+            const result =  await conn.query( query );
+
+            res.json( result );
+        } catch (error) {
+            res.json({
+                status: 'failed',
+                message: 'Error al crear producto en almacen: ' + error
+            });
+        }
     },
-    getProductoAlmacen:  ( req, res ) => {
-        res.json('Producto en almacen');
+    getProductoAlmacen: async ( req, res ) => {
+        try {
+            const query = `SELECT * FROM almacen WHERE id_almacen = ${ req.params.id }`;
+
+            const conn = await getConnection();
+            const result =  await conn.query( query );
+
+            res.json( result );
+        } catch (error) {
+            res.json({
+                status: 'failed',
+                message: 'Error al obtener el producto en almacen: ' + error
+            });
+        }
     },
-    updateAlmacenProducto: ( req, res ) => {
-        res.json('Actualizado');
+    updateAlmacenProducto: async ( req, res ) => {
+        console.log( req.body )
+        try {
+            const query = `
+                UPDATE almacen SET
+                    producto = "${ req.body.producto }",
+                    tipo_producto = "${ req.body.tipo_producto }",
+                    cantidad = ${ req.body.cantidad }
+                WHERE id_almacen = ${ req.params.id };
+            `;
+
+            const conn = await getConnection();
+            const result = await conn.query( query );
+
+            res.json( result );
+
+        } catch (error) {
+            res.json({
+                status: 'failed',
+                message: 'Error al actualizar producto en almacen: ' + error
+            });   
+        }
     },
-    deleteAlmacenProducto: ( req, res ) => {
-        res.json('Producto en almacen eliminado');
+    deleteAlmacenProducto: async ( req, res ) => {
+        try {
+            const conn = await getConnection();
+            const result = await conn.query(`DELETE FROM almacen WHERE id_almacen = ${ req.params.id };`);
+            res.json( result );
+        } catch (error) {
+            res.json({
+                status: 'failed',
+                message: 'Error al eliminar producto en almacen: ' + error
+            });
+        }
     },
     
- };
+};
--- src/public/js/almacen.js
+++ src/public/js/almacen.js
@@ -1,9 +1,64 @@
+console.log('--- almacen.js ---')
+
+const { getToServer, putToServer } = require('./js/helpers/llamadas');
+const $ = require('jquery');
+
 (() => {
-    console.log('--- almacen.js ---')
 
-    const bntGuardar = document.querySelector('#btnGuardar');
-    bntGuardar.addEventListener('click', () => {
+    getToServer('almacen')
+        .then(almacen => {
+            console.log( almacen )
+
+            $.each(almacen, ( i, producto ) => {
+                $('#areaProductos').append(`
+                    <div class="col-sm-3 p-5 mr-2">
+                        <div class="card mx-auto" style="width: 23rem;">
+                            <input name="id-almacen" type="hidden" value=${ producto.id_almacen }>
+                            <img src="${ producto.url }" class="card-img-top" alt="${ producto.producto }">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">${ producto.producto }</h5>
+                                <p class="card-text p-0 m-0">Cantidad en almacén</p>
+                                <p class="card-text p-0 m-0">(Paquetes)</p>
+                                <p class="card-text p-0 m-0">${ producto.cantidad }</p>
+                                <a class="btn btn-vino py-2 mt-4 mx-2 btn-editar" data-bs-toggle="modal" data-bs-target="#exampleModal2" data-bs-whatever="@mdo">EDITAR</a>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            });
+
+            $('.btn-editar').on('click', ( e ) => {
+                const that = $(e.currentTarget);
+                const id = that.parents('.col-sm-3.p-5.mr-2').find('input').val();
+                
+                getToServer(`almacen/${ id }`)
+                    .then(data => {
+                        const producto = data[0];
+                        $('#idInfo').val( producto.id_almacen );
+                        $('#productoInfo').val( producto.producto );
+                        $('#tipoProductoInfo').val( producto.tipo_producto );
+                        $('#cantidadInfo').val( producto.cantidad );
+                    })
+
+                
+                $('#btnActualizar').on('click', () => {
+                    const data = {
+                        producto: $('#productoInfo').val(),
+                        tipo_producto: $('#tipoProductoInfo').val(),
+                        cantidad: $('#cantidadInfo').val()
+                    }
+                    putToServer(`almacen/${ id }`, data)
+                        .then(resp => {
+                            // console.log( resp )
+                            alert('Almacen actualizado')
+                            setTimeout(() => {
+                                window.location.reload();
+                            }, 500);
+                        })
+                });
+            });
         
-    });
 
+        })
+        .catch( err => console.log );
 })();
